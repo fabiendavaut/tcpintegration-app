c7168801d8c46ba73911af4ad687c3eb1a22bcb3
Davaut Fabien <fabien.davaut@viadialog.com>
2018-06-19 11:42:45 +0200

    change the default BusConfig

--- src/main/java/com/viadialog/tcpintegration/integration/BusConfigService.java
+++ src/main/java/com/viadialog/tcpintegration/integration/BusConfigService.java
@@ -28,11 +28,10 @@ public class BusConfigService {
 
         BusConfig busConfig = new BusConfig();
         busConfig.setId(1L);
-        busConfig.setHostname("localhost");
-        busConfig.setPort(8080);
-        busConfig.setModule("MyModule");
-        busConfig.setFamily("jhkjh");
-
+        busConfig.setHostname("vlz2-viapika-dev.velizy.lan");
+        busConfig.setPort(9000);
+        busConfig.setModule("APP_TEST");
+        busConfig.setFamily("VIACOMAPPS2");
         busConfigList.add(busConfig);
 
         return busConfigList;
